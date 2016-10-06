9ec0644347f404198486cae9df25d5b37a06344f
DarkWiiPlayer <darkwiiplayer@hotmail.com>
2016-10-06 13:37:47 +0200

    Improved the layout a bit, redid some of the key library functions

--- app.lua
+++ app.lua
@@ -1,12 +1,13 @@
-local lapis = require("lapis")
+local lapis = require "lapis"
 local configuration = require("lapis.config").get()
 local app = lapis.Application()
+--local keys = require "keys"
 
 app:enable("etlua")
 app.layout = false
 
 app:before_filter(function(self)
-  self.nav = {"Front page", "About"}
+  self.nav = {"Front page", "About", "Download"}
   self.configuration = configuration;
 end)
 
@@ -23,7 +24,41 @@ app:get("front_page", "/", function(self)
   }
 end)
 
-app:get("file", "/file/*", function(self)
+--[[
+#==Download Section==
+This is where files are actually downloaded
+See _file section_ for information about files
+--]]
+
+app:get("download", "/download", function(self)
+  return {
+    self:html(function(self)
+      h1 "Download section"
+      p "This is where you download files (duh)"
+    end);
+    layout = "layout";
+  }
+end)
+
+app:get("download_file", "/download/*", function(self)
+  
+  -- Start by checking permissions
+  if false then -- todo: add files that don't need code
+    return {"Error 724: This line should be unreachable!", status=724}
+  elseif not self.params.key then
+    return { self:html(function()
+      h1 "Access Denied"
+      p "Please provide a key with access clearance for this file!"
+      end);
+      layout = "layout";
+    }
+  elseif not true then
+    return "fuck you"
+  end
+  
+  -- Try gzipped files first
+
+  -- |important| todo: check for Accept-Encoding header
   local file, err, errn = io.open("files/"..self.params.splat..".gz")
   if file then
     local input = file:read "*a"
@@ -35,6 +70,8 @@ app:get("file", "/file/*", function(self)
       };
     }
   end
+  
+  -- Try normal file if no gzipped file can be found
   
   file, err, errn = io.open("files/"..self.params.splat)
   if file then
@@ -48,12 +85,33 @@ app:get("file", "/file/*", function(self)
   end
 end)
 
--- About dl_Keys
+
+
+--[[
+#==File section==
+This is where the user will see information about files and get a download link
+|todo|: implement /lol
+--]]
+
+app:get("file", "/file/*", function(self)
+  return {
+    self:html(function(self)
+      p1 "this section is totally not implemented :P"
+    end);
+  }
+end)
+
+
+
+--[[
+#==About page==
+feel free to change this, but of course a link to my [website](http://www.darkwiiplayer.com/) is always welcome :)
+--]]
 
 app:get("about", "/about", function(self)
   return {self:html(function(env)
     h1 "About dl_Keys"
-    p "dl_Keys is a web-app built on top of the lapis web framework that serves files for downloads pretected by download keys."
+    p "dl_Keys is a web-app built on top of the lapis web framework that serves files for downloads protected by download keys."
     p(function() 
       text "Author: "
       a{"DarkWiiplayer", href="//darkwiiplayer.com"}
--- keys.lua
+++ keys.lua
@@ -1,33 +1,152 @@
 local lib = {}
 
 --[[
-  KEY FORMAT
+#   Key format:
   {
+    path:string
     key:string
-    expires:date
-    created:date
+    starts:date
+    ends:date
+    max_clicks:n
+    clicks:n
   }
+  
+#   Todo list:
+  2016-10-06
+  - Read Keys --done
+  - Write Keys --done
+  - `__tostring` metamethod for keys --done
+  - implement key object integrity check
 --]]
 
-function lib.get(directory)
-  -- Given a directory, returns a list of all keys that apply to it
-  -- TODO: Implement
-  return {{key="master";expires=0;created=0}} -- TODO: add data!
+-- **Data Access / Back-end**
+
+local fields = {"path", "key", "starts", "ends", "max_clicks", "clicks"}
+
+local _key_meta = {
+  __tostring = function(self)
+    local result = ""
+    for index, value in ipairs(fields) do
+      result = result .. value .. " = " .. (self[value] or "")
+      if index < #fields then result = result .. "\n" end
+    end
+    return result
+  end;
+}
+
+local function genKey(length)
+  math.randomseed(os.time())
+  local str = ""
+  for i=1,length do
+    str = str..("%X"):format(math.random(0,255))
+  end
+  return str
 end
 
-function lib.add(directory, Key)
-  local key, expires
-  if Key then
-    key = Key.key or "master" -- TODO: add key generation
-    expires = Key.expires or 0 -- TODO: add default (current + read from settings)
-  else
-    -- TODO: Same as above
-    key = "master"
-    expires = 0
+local function check_data_integrity(key)
+  -- Key must have all the needed fields fields
+  for index, field in ipairs(fields) do
+    if not key[field] then return false end
   end
   
-  -- Given a directory, adds a key to it
-  return "master" -- TODO: Should return the newly generated key
+  -- Key must not have expired
+  if (tonumber(key.ends) or 0) < os.time() then return false end
+  
+  -- Key must not have too many clicks
+  if (tonumber(key.max_clicks) or 0)>0 then
+    if (tonumber(key.clicks) or 0) > tonumber(key.max_clicks) then return false end
+  end
+  
+  return true
 end
+
+local function read_keys(filename)
+  local keys = {}
+  local file = io.open(filename)
+  if not file then return nil, "Could not open key file for reading" end
+  for line in file:lines() do
+    local key = setmetatable({}, _key_meta)
+    local i=1; for thing in line:gmatch"[^\t]+" do
+      if fields[i] then key[fields[i]]=thing end
+      i = i + 1
+    end
+    table.insert(keys, key)
+  end
+  file:close()
+  
+  return keys
+end
+
+local function write_keys(filename, keys)
+  local file = io.open(filename, "w")
+  if not file then return nil, "Could not open file" end
+  ----
+  for _, key in ipairs(keys) do
+    if check_data_integrity(key) then
+      for index, field in ipairs(fields) do
+        file:write(key[field])
+        if index<#fields then file:write "\t" end
+      end
+      file:write("\n")
+    end
+  end
+  ----
+  file:close()
+  return true;
+end
+
+local function match_dir(path, directory)
+  return directory:find("^/?"..path:match"^/?(.-)/?&")
+end
+
+-- **Interface / Front-end**
+
+local keyfile = ".keys"
+
+function lib.add(directory, key)
+  local keys,err = read_keys(keyfile)
+  if not keys then return nil, err end
+  
+  table.insert(keys, key)
+  --table.sort(keys, function(a,b) return a.dir<b.dir end) -- **TODO**: Improve
+  
+  print("Writing "..#keys.." keys...")
+  return write_keys(keyfile, keys)
+end
+
+function lib.remove(filter)
+  
+  local keys,err = read_keys(keyfile)
+  if not keys then return nil, err end
+  
+  local result = {}
+  for _, key in ipairs(keys) do
+    if (not filter.path or filter.path==key.path) and
+    (not filter.key or filter.path==key.key) and
+    (not filter.expires or filter.path==key.expires) and
+    (not filter.created or filter.path==key.created) then
+      table.insert(result,key)
+    end
+  end
+  
+  return write_keys(keyfile, result) -- this _should_ lead to a proper tail call
+end
+
+function lib.can_access(keystr, directory)
+  -- |broken| todo: actually check that the directory maches too
+  -- |broken| todo: check that the key is actually valid!
+  local keys = read_keys(keyfile)
+  for index, key in ipairs(keys) do
+    if keystr == key.key then return true end
+  end
+  return false
+end
+
+
+local k = read_keys(keyfile) or {}
+k[#k+1]=setmetatable({path="/", key=genKey(10), starts=os.time(), ends=os.time()+20, max_clicks=20, clicks=0}, _key_meta)
+write_keys(keyfile, k)
+print(#k)
+print(k[#k])
 
 return lib
